fe3201f65613c73109829fbf1b833962bd252d7e
Naoto Yoshida <n-yoshida@isi.imi.i.u-tokyo.ac.jp>
2022-11-20 19:06:02 +0900

    refactoring

--- atari.py
+++ atari.py
@@ -36,16 +36,15 @@ def get_batch(batch_size):
 
 if __name__ == '__main__':
     
-    dim_latent = 20
-    n_category = 50
-    # n_query = 10
+    dim_latent = 10  # 512
+    n_category = 20  # 512
     
     # model = ConvAE(dim_latent=dim_latent)
     # model = ConvVAE(dim_latent=dim_latent)
     model = ConvVQVAE(dim_latent=dim_latent, n_category=n_category)
     # model = ConvVQVAECos(dim_latent=dim_latent, n_category=n_category)
 
-    optimizer = torch.optim.AdamW(params=model.parameters(), lr=0.001)
+    optimizer = torch.optim.AdamW(params=model.parameters(), lr=0.003)
     
     n_itr = 10000
     
@@ -56,7 +55,7 @@ if __name__ == '__main__':
         
         model.train()
         
-        x = get_batch(64)
+        x = get_batch(128)
         
         _, _, loss, _ = model(x)
         
@@ -74,14 +73,14 @@ if __name__ == '__main__':
             with torch.no_grad():
                 x_test = get_batch(1)
                 x_pred, z, _, z_index = model(x_test)
-            
-            plt.clf()
-            
+                
             plt.subplot(221)
+            plt.cla()
             plt.imshow(einops.rearrange(x_test, "b c h w -> h (b w) c").numpy())
             plt.title("input")
             
             plt.subplot(222)
+            plt.cla()
             if isinstance(model, ConvAE) or isinstance(model, ConvVAE):
                 plt.imshow(z, cmap="gray")
             elif isinstance(model, ConvVQVAE) or isinstance(model, ConvVQVAECos):
@@ -89,10 +88,12 @@ if __name__ == '__main__':
             plt.title("latent")
             
             plt.subplot(223)
+            plt.cla()
             plt.imshow(einops.rearrange(x_pred, "b c h w -> h (b w) c").numpy())
             plt.title("reconstruction")
             
             plt.subplot(224)
+            plt.cla()
             plt.plot(hist_loss)
             plt.yscale("log")
             plt.title("Loss")
